9f02bb5e05974b56cf1538c843bd8e78c7a6da75
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2022-02-03 11:16:37 +0300

    feat: add function writeSymbol

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log('hi');
+const gameField = document.querySelector('.game-field');
+const sells = gameField.querySelectorAll('.sell');
+let isX = true;
+
+console.log(sells);
+
+
+function writeSymbol(ev) {
+    
+    if (ev.target.classList.contains('sell') && !ev.target.classList.contains('selected')) {
+        if (isX) {
+            ev.target.textContent = 'x';
+            ev.target.classList.add('selected');
+            isX = false;
+        } else {
+            ev.target.textContent = '0';
+            ev.target.classList.add('selected');
+            isX = true;
+        }
+    }
+}
+
+gameField.addEventListener('click', writeSymbol);
